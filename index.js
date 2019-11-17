6e9d808915b64c9073fc60be7411cf9b9c1fded3
Eric Brock <ebrockk@gmail.com>
2019-11-17 11:10:39 -0500

    style guides airbnb

--- index.js
+++ index.js
@@ -1,42 +1,46 @@
 const Discord = require('discord.js');
+
 const client = new Discord.Client();
 const config = require('./config.json');
+const giphy = require('giphy-api')(config.giphy);
 
-client.on("ready", () => {
-    console.log(`Logged in as ${client.user.tag}!`)
-    console.log(client.users.array()); // print all users in server.
-    // Test guildMemberAdd.
-    let user = client.users.get("283737656247189524");
-    client.emit("guildMemberAdd", user);
-})
-
-// Simple test. 
-client.on("message", msg => {
-    if (msg.content === "ping") {
-        msg.reply("pong!");
-    }
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+  console.log(client.users.array()); // print all users in server.
+  // Test guildMemberAdd.
+  const user = client.users.get('283737656247189524');
+  client.emit('guildMemberAdd', user);
 });
 
+// Simple test.
+client.on('message', (msg) => {
+  if (msg.content === 'ping') {
+    msg.reply('pong!');
+  }
+});
 
 // Command test.
-client.on("message", msg => {
-    // If no prefix, ignore (return).
-    if (msg.content.indexOf(config.prefix) !== 0) return;
+client.on('message', msg => {
+  // If no prefix, ignore (return).
+  if (msg.content.indexOf(config.prefix) !== 0) return;
 
-    // Parse message.
-    // const words = msg.content.slice(1);
-    const command = msg.content.slice(1).split(" ")[0].toLowerCase();
-    const user = msg.mentions.users.first();
-    console.log(`cmd: ${command}, user: ${user.username}`);
+  // Parse message.
+  // const words = msg.content.slice(1);
+  const command = msg.content.slice(1).split(' ')[0].toLowerCase();
+  const user = msg.mentions.users.first().username;
+  console.log(`cmd: ${command}, user: ${user}`);
 
-    // If slap...
-    if (command === "slap") {
-        msg.channel.send(`${msg.author} slapped ${user}`);
-    }
+  // If slap...
+  if (command === 'slap') {
+    msg.channel.send(`**${msg.author.username}** slapped **${user}**`);
+    giphy.random('slap').then((res) => {
+      msg.channel.send(res);
+    });
+  }
 });
 
-client.on("guildMemberAdd", (member) => {
-    console.log(`${member} has joined the server!`);
-})
+client.on('guildMemberAdd', (member) => {
+  console.log(`${member} has joined the server!`);
+});
 
-client.login(config.test)
+client.login(config.test);
